07d1e8825f686b6f91cb5528801a43814a86e3f2
nrossite <112349350+nrossite@users.noreply.github.com>
2022-09-19 16:56:35 -0400

    Add files via upload
    Uploaded bash script from HW1 problem 2

---
+++ b575hw12b_script.sh
@@ -0,0 +1,14 @@
+echo '#== SCRIPT STARTED'
+
+awk "NR%4==2" sample.fastq > sample_rseq.txt
+awk "NR%4==2" sample_cutadapt.fastq > sample_cutadapt_rseq.txt 
+
+echo "#== RETRIEVE READ SEQUENCE DONE"
+
+sed "s/AGATCGGAAGAGC.*//" sample_rseq.txt > sample_rseq_CUT.txt
+
+echo "#== CUT ADAPTER DONE"
+
+diff sample_rseq_CUT.txt sample_cutadapt_rseq.txt
+
+echo "#== SCRIPT DONE"
